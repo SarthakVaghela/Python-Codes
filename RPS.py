be41fc1fb14db8012310b652094cd4e1b6db02fa
Sarthak Vaghela <76500387+SarthakVaghela@users.noreply.github.com>
2021-01-27 17:57:54 -0500

    Update RPS.py

--- RPS.py
+++ RPS.py
@@ -6,22 +6,22 @@
 
 import sys
 
-def compare(p1,p2):
-    if p1 == p2:
+def compare(p1,p2):                 #creating a method for us to compare two inputs
+    if p1 == p2:                    # condition for tie
         return("Its a tie!")
-    elif p1 == 'rock':
+    elif p1 == 'rock':              #condition for rock
         if p2 == 'paper':
             return("Paper WINS!")
         else:
             return("Rock WINS")
 
-    elif p1 == 'paper':
+    elif p1 == 'paper':             #condition for paper
         if p2 == 'scissors':
             return("Scisorrs WINS!")
         else:
             return("Paper WINS!")
     
-    elif p1 == 'scissors':
+    elif p1 == 'scissors':          #conditiopn for paper
         if p2 == 'rock':
             return("Rock WINS!")
         else:
@@ -32,12 +32,12 @@ def compare(p1,p2):
 
     
 
-while True:
+while True:                        # infinite loop in order for the user to play this game as much as he wants
     player1 = input("please enter your name: ")
     player2 = input("please enter your name: ")
 
-    p1_answer = input("%s, please choose from rock, paper or scissors: " %player1)
-    p2_answer = input("%s, please choose from rock paper ot scissors: " %player2)
+    p1_answer = input("%s, please choose from rock, paper or scissors: " %player1) #taking input from user1
+    p2_answer = input("%s, please choose from rock paper ot scissors: " %player2) #taking input for user2
     print(compare(p1_answer, p2_answer))
-    x = input("Press any key to continue\nor type done to exit: ")
+    x = input("Press any key to continue\nor type done to exit: ") #asking user to play the game again
     if x == 'done': break
